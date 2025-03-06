a6223d1e937d944e1c5f65785dca5afc648af300
Edenbdd <aubertra@student.42.fr>
2025-03-06 13:48:53 +0100

    ex02 done

---
+++ ex02/main.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aubertra <aubertra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/06 13:44:08 by aubertra          #+#    #+#             */
+/*   Updated: 2025/03/06 13:47:50 by aubertra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string brain = "HI THIS IS BRAIN";
+    std::string *stringPTR;
+    std::string &stringREF = brain;
+
+    stringPTR = &brain;
+
+    std::cout << "Adresses: " << std::endl;
+    std::cout << "Brain: " << &brain << std::endl;
+    std::cout << "stringPTR: " << stringPTR << std::endl;
+    std::cout << "stringREF: " << &stringREF << std::endl;
+    std::cout << "Variables: " << std::endl;
+    std::cout << "Brain: " << brain << std::endl;
+    std::cout << "stringPTR: " << *stringPTR << std::endl;
+    std::cout << "stringREF: " << stringREF << std::endl;
+
+    return (0);
+}
